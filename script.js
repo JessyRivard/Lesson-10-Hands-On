1b5289f5524a078c5d7f0a3837a233ab13cb6f78
Jessy Rivard <jessyrivard@icloud.com>
2018-09-30 16:43:55 -0400

    Final touches
    repaired a typo in js code, restyled links in github repos list, removed unnecessary link in navbar, resized bootstrap columns, repaired error message.

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ pageLoad.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let repos = JSON.parse(this.responseText);
         for (var i = 0; i < repos.length; i++) {
-            $("#repos").innerHTML = "";
-            $("#repos").append("<ul class=\"repoitem\"><li>Name: <a href=\"" + repos[i].html_url + "\">" + repos[i].name + "</a></li><li>Description: " + repos[i].description + "</li><li>Language: " + repos[i].languages + "</li></ul>");
+            $(".error").remove();
+            $("#repos").append("<br></br><ul class=\"repos\"><li><b>Name:</b> <a href=\"" + repos[i].html_url + "\">" + repos[i].name + "</a></li><li><b>Description:</b> " + repos[i].description + "</li><li><b>Language:</b> " + repos[i].language + "</li></ul>");
         };
     };
 };
