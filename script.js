e0c481eada11ec466d341cf12666de91ec8854b3
Jessy Rivard <jessyrivard@icloud.com>
2018-09-30 16:19:53 -0400

    Added footer/ cleaned up code
    Changed the way the language is pulled from the API, and simplified the results, added a footer with email to the bottom of the page, added some styling elements for the footer.

--- script.js
+++ script.js
@@ -12,16 +12,8 @@ pageLoad.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let repos = JSON.parse(this.responseText);
         for (var i = 0; i < repos.length; i++) {
-            let languages = new XMLHttpRequest();
-            languages.onreadystatechange = function(){
-                if (this.readyState == 4 && this.status == 200) {
-                    let lang = JSON.parse(this.responseText);
-                    return Object.keys(lang)[0];
-                };
-            };
-            languages.open("GET", "https://api.github.com/repos/JessyRivard/" + repos[i].name + "/languages", true);
-            languages.send();
-            $("#repos").append("<ul class=\"repoitem\"><li>Name: <a href=\"" + repos[i].html_url + "\">" + repos[i].name + "</a></li><li>Description: " + repos[i].description + "</li><li>Language: " + languages + "</li></ul>");
+            $("#repos").innerHTML = "";
+            $("#repos").append("<ul class=\"repoitem\"><li>Name: <a href=\"" + repos[i].html_url + "\">" + repos[i].name + "</a></li><li>Description: " + repos[i].description + "</li><li>Language: " + repos[i].languages + "</li></ul>");
         };
     };
 };
